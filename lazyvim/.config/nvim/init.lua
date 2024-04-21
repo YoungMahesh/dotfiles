184574560f34971c8330308f542fb9adbd08ed8e
mahesh <maheshghamand@gmail.com>
2024-04-21 11:47:55 +0530

    lazyvim - autocomplete, comment, colorscheme

--- lazyvim/.config/nvim/init.lua
+++ lazyvim/.config/nvim/init.lua
@@ -1,3 +1,4 @@
 -- import .lua file using "require", we can ignore folder name 'lua' here
 -- if .core is folder-name, lua will look for init.lua file in it
 require("mahesh.core")
+require("mahesh.lazy")
---
+++ lazyvim/.config/nvim/lua/mahesh/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+-- editor tab design
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "tabs",
+      separator_style = "slant",
+    },
+  },
+}

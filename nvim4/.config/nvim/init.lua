57e0998cef24e82fdf6634fd9a45c9b4f77e0180
mahesh <maheshghamand@gmail.com>
2024-05-09 15:02:31 +0530

    neovim upgrade

--- nvim4/.config/nvim/init.lua
+++ nvim4/.config/nvim/init.lua
@@ -1,3 +1,6 @@
+-- installataion and upgrade: https://github.com/neovim/neovim/blob/master/INSTALL.md#pre-built-archives-2
+-- nvim -v: 0.9.5
+-- :checkhealth
 require("mahesh.core")
 require("mahesh.lazy")
 

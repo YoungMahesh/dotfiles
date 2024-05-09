49a3d6a0954b6457edc18ed48e560fcbaaedbbb1
mahesh <maheshghamand@gmail.com>
2024-05-09 11:13:41 +0530

    options

--- nvim4/.config/nvim/lua/mahesh/core/options.lua
+++ nvim4/.config/nvim/lua/mahesh/core/options.lua
@@ -1,2 +1,12 @@
-print('hello from mahesh/core/options')
+local opt = vim.opt
 
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+opt.tabstop = 2 -- spaces for tabs
+opt.shiftwidth = 2 -- spaces for indent width
+
+opt.number = true
+opt.wrap = false
+
+-- incremental-search: highlight text 'while typing' search text with :/<search-text>
+opt.incsearch = true

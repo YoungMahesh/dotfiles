9c3537965a23b66562f4817b244f9372d2dc7fa7
mahesh <maheshghamand@gmail.com>
2024-05-11 10:07:32 +0530

    removed legancy neovim configuration

--- lazyvim/.config/nvim/init.lua
+++
@@ -1,4 +0,0 @@
--- import .lua file using "require", we can ignore folder name 'lua' here
--- if .core is folder-name, lua will look for init.lua file in it
-require("mahesh.core")
-require("mahesh.lazy")

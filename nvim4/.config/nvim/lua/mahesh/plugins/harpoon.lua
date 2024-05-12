d76934bf6b8655ddcfffa76cb788a1f20949fe89
mahesh <maheshghamand@gmail.com>
2024-05-12 14:17:58 +0530

    harpoon keymap update

--- nvim4/.config/nvim/lua/mahesh/plugins/harpoon.lua
+++ nvim4/.config/nvim/lua/mahesh/plugins/harpoon.lua
@@ -11,20 +11,21 @@ return {
     --
 
     -->h == harpoon, ha=harpoon add, he=harpoon explore
-    vim.keymap.set("n", "ha", function() harpoon:list():add() end, { desc = 'Add current file to harpoon' })
+		-- not using `h` directory, as h is part of vim-motion to move cursor to left
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = 'Add current file to harpoon' })
     -- {open harpoon list and then type `dd`, then `:w`, desc: 'Remove file from list'}
     -- {open harpoon list and then type `dd`, then `p` on another file-name, desc: 'Rearrange position of file in list'}
-    vim.keymap.set("n", "he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+    vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
       { desc = 'Open harpoon list' })
 
     --> Shift+a -> already mapped at append at end of the line
-    vim.keymap.set("n", "h1", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "h2", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "h3", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "h4", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "hp", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "hn", function() harpoon:list():next() end)
+    vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
   end,
 }

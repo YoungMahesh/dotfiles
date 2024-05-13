b9645252024f6939d5b8e940b1b67f1e5b38294a
mahesh <maheshghamand@gmail.com>
2024-05-13 22:18:25 +0530

    telescope keymap update

--- nvim4/.config/nvim/lua/mahesh/plugins/telescope.lua
+++ nvim4/.config/nvim/lua/mahesh/plugins/telescope.lua
@@ -6,17 +6,23 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
-    --> p == project, pf=project-files,
+    local keymap = vim.keymap
+    -->f == find, fl=find-list, fs=find-string, fb=find-buffer, fh=find-help, fr=find-recent
     -- ps=project-search, search current file
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+    keymap.set('n', '<leader>fl', builtin.find_files, { desc = 'Fuzzy find files in cwd' })
+    keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Find string in cwd' })
+    keymap.set('n', '<leader>fb', builtin.buffers, {})
+    keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Fuzzy find recent files' })
+    keymap.set('n', '<leader>ff', '<cmd>Telescope grep_string<cr>', { desc = 'Find string under cursor in cwd' })
     -- vim.keymap.set('n', '<leader>ps', function()
     --  builtin.grep_string({ search = vim.fn.input("Grep > ") });
     -- end)
     -- <C-p> = Ctrl+p
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    keymap.set('n', '<C-p>', builtin.git_files, {})
+
+
+
     --vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     --vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     --vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})

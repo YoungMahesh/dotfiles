36f9f5cf18e5e7bcdaf9ffc94bc61d1b0b9a5be6
mahesh <maheshghamand@gmail.com>
2024-05-13 18:05:39 +0530

    telescope-keymaps

--- nvim4/.config/nvim/lua/mahesh/plugins/telescope.lua
+++ nvim4/.config/nvim/lua/mahesh/plugins/telescope.lua
@@ -1,20 +1,24 @@
--- :checkhealth telescope 
+-- :checkhealth telescope
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-	      local builtin = require('telescope.builtin')
-	  --> p == project, pf=project-files, 
-	  -- ps=project-search, search current file
-	vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-	vim.keymap.set('n', '<leader>ps', function()
-		builtin.grep_string({ search = vim.fn.input("Grep > ") });
-	end)
-	-- <C-p> = Ctrl+p
-	vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-	--vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-	--vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-	--vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-end,
-    }
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.6',
+  -- or                              , branch = '0.1.x',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local builtin = require('telescope.builtin')
+    --> p == project, pf=project-files,
+    -- ps=project-search, search current file
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+    -- vim.keymap.set('n', '<leader>ps', function()
+    --  builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    -- end)
+    -- <C-p> = Ctrl+p
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    --vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    --vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    --vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+  end,
+}

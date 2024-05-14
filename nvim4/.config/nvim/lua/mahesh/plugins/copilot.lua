f97cc6a1463292b65a167ecf2655effbfcea9e87
mahesh <maheshghamand@gmail.com>
2024-05-15 04:32:25 +0530

    copilot keymaps

--- nvim4/.config/nvim/lua/mahesh/plugins/copilot.lua
+++ nvim4/.config/nvim/lua/mahesh/plugins/copilot.lua
@@ -4,4 +4,10 @@ return {
 -- :help copilot
 	-- :Copilot disable
 	-- :Copilot enable
+
+	config = function()
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>cd", "<cmd>Copilot disable<cr>", { })
+		keymap.set("n", "<leader>ce", "<cmd>Copilot enable<cr>", { })
+	end,
 }

4dedbb9726ba0104694d90ccf7253e867fe3af40
mahesh <maheshghamand@gmail.com>
2024-05-22 07:07:03 +0530

    fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,31 @@
+# install on ubuntu: sudo apt install fish
+# docs: https://fishshell.com/
+# stopped using fish, as command color like `ls` is dark blue which is unvisible on black background
+#set -g fish_color_command white
+#set -g fish_color_keyword white
+#set -g fish_color_quote yellow
+#set -g fish_color_redirection white
+#set -g fish_color_end white
+#set -g fish_color_error red
+#set -g fish_color_param white
+#set -g fish_color_comment cyan
+#set -g fish_color_selection white
+#set -g fish_color_search_match white
+#set -g fish_color_operator white
+#set -g fish_color_escape white
+#set -g fish_color_autosuggestion white
+#set -g fish_color_cancel white
+#set -g fish_color_cwd white
+#set -g fish_color_user white
+#set -g fish_color_host white
+#set -g fish_color_match white
+#set -g fish_color_selection white
+#set -g fish_color_search_match white
+#set -g fish_color_operator white
+
+
+# install fisher: https://github.com/jorgebucaran/fisher
+
+# install theme (https://github.com/catppuccin/fish)
+# fisher install catppuccin/fish
+#fish_config theme save "Catppuccin Mocha"

c9fc20b75570c37d85831e679bf4ddebed3e0f4f
mahesh <maheshghamand@gmail.com>
2024-05-22 09:56:53 +0530

    minor updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,33 @@
 # install on ubuntu: sudo apt install fish
 # docs: https://fishshell.com/
 
+# set fish as default terminal
+# 1. get path-of-fish: which fish 
+# 2. check if fish is part of acceptable shells: cat /etc/shells
+# if not, then add it to list of acceptable shells: echo <path-of-fish> | sudo tee -a /etc/shells
+# 3. change default shell defualt shell: chsh -s <path-of-fish> 
+# 4. logout and login again
+# 5. open terminal and verify default shell: echo $SHELL
+
+# switch temporarily to bash
+# 1. bash
+# 2. echo $0   # if output is "bash", then you are in bash terminal
+
 set -g fish_color_command white # default is dark-blue which is unreadable
 
 # accept suggestions with tab, default is: Ctrl+f
 bind \t accept-autosuggestion
 
 # neovim
+# neovim lsp does not work with fish, switch to using `bash` and then use nvim
 export PATH="$PATH:/opt/nvim-linux64/bin"
 
 starship init fish | source
 
+# messages when we open terminal
+set -U fish_greeting "rejecting failure == rejecting success"
 
-zoxide init fish | source
+
 
 #set -g fish_color_keyword white
 #set -g fish_color_quote yellow

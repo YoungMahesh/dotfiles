d65de84fc7bed9119868d94f0608f2f3191f990f
mahesh <maheshghamand@gmail.com>
2024-06-02 07:34:43 +0530

    alacritty fullscreen, ubuntu-docs, tmux-scripts

--- neovim/.config/nvim/lua/mahesh/plugins/copilot.lua
+++ neovim/.config/nvim/lua/mahesh/plugins/copilot.lua
@@ -9,5 +9,8 @@ return {
 		local keymap = vim.keymap
 		keymap.set("n", "<leader>cd", "<cmd>Copilot disable<cr>", { })
 		keymap.set("n", "<leader>ce", "<cmd>Copilot enable<cr>", { })
+
+		-- disable by default
+		vim.cmd("Copilot disable")
 	end,
 }
---
+++ tmux/.tmux/nftclash-setup.bash
@@ -0,0 +1,26 @@
+# -d starts a new session in detached mode
+# -s specifies the session name
+# -n specifies the window name
+# C-m is the Enter key
+# -t specifies the target pane
+# -h splits the window horizontally
+
+# ncf-vim
+tmux new-session -d -s nc -n 'ncf-vim'
+tmux send-keys -t nc:ncf-vim 'cd ~/company/nft-clash/apps/frontend' C-m
+
+tmux new-window -t nc:1 -n ncc
+tmux send-keys -t nc:ncc 'cd ~/company/nft-clash/contracts' C-m
+
+tmux new-window -t nc:2 -n localhost
+tmux send-keys -t nc:localhost 'cd ~/company/nft-clash/apps/frontend' C-m
+tmux split-window -h -t nc:localhost
+tmux send-keys -t nc:localhost.1 'cd ~/company/nft-clash/contracts' C-m
+tmux send-keys -t nc:localhost.1 'bash start.localnet.bash' C-m
+
+tmux new-window -t nc:3 -n note
+tmux send-keys -t nc:note 'nvim note' C-m
+
+tmux select-window -t nc:ncf-vim
+tmux attach-session -t nc
+

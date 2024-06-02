34b79bccdedc9a016ff1a88469fbd958c56fab09
mahesh <maheshghamand@gmail.com>
2024-06-02 14:39:32 +0530

    neovim: uninstalled copilot

--- neovim/.config/nvim/lua/mahesh/plugins/copilot.lua
+++ neovim/.config/nvim/lua/mahesh/plugins/copilot.lua
@@ -1,16 +1,16 @@
 return {
-'github/copilot.vim',
--- :Copilot setup
--- :help copilot
-	-- :Copilot disable
-	-- :Copilot enable
-
-	config = function()
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>cd", "<cmd>Copilot disable<cr>", { })
-		keymap.set("n", "<leader>ce", "<cmd>Copilot enable<cr>", { })
-
-		-- disable by default
-		vim.cmd("Copilot disable")
-	end,
+  --'github/copilot.vim',
+  ---- :Copilot setup
+  ---- :help copilot
+  --	-- :Copilot disable
+  --	-- :Copilot enable
+  --
+  --	config = function()
+  --		local keymap = vim.keymap
+  --		keymap.set("n", "<leader>cd", "<cmd>Copilot disable<cr>", { })
+  --		keymap.set("n", "<leader>ce", "<cmd>Copilot enable<cr>", { })
+  --
+  --		-- disable by default
+  --		vim.cmd("Copilot disable")
+  --	end,
 }

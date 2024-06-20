24e74ea84bc67f02ea056a09f22ed682b7e1bcf0
mahesh <maheshghamand@gmail.com>
2024-06-20 16:15:22 +0530

    neovim: solidity lsp

--- neovim/.config/nvim/lua/mahesh/plugins/lsp/lspzero.lua
+++ neovim/.config/nvim/lua/mahesh/plugins/lsp/lspzero.lua
@@ -110,7 +110,7 @@ return {
 
       require('mason-lspconfig').setup({
         -- available servers: https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-        ensure_installed = { "lua_ls", "tsserver", "prismals", "gopls", "clangd"},
+        ensure_installed = { "lua_ls", "tsserver", "prismals", "gopls", "clangd", "solidity_ls_nomicfoundation" },
         handlers = {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"

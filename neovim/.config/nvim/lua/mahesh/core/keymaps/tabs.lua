7c075d41da83165dc8d354bf4bfc9674b39e2e3a
mahesh <maheshghamand@gmail.com>
2024-10-03 02:06:55 +0530

    disable neovim: which-key

--- neovim/.config/nvim/lua/mahesh/core/keymaps/tabs.lua
+++ neovim/.config/nvim/lua/mahesh/core/keymaps/tabs.lua
@@ -5,4 +5,4 @@ keymap.set("n", "tq", "<cmd>tabclose<cr>", { desc = "close current tab" })
 keymap.set("n", "tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "tc", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
-
+-- 2gt -> switch to 2nd tab, 4gt -> switch to 4th tab, etc
--- neovim/.config/nvim/lua/mahesh/plugins/which-key.lua
+++ neovim/.config/nvim/lua/mahesh/plugins/which-key.lua
@@ -1,20 +1,21 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- https://github.com/folke/which-key.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
-    triggers = {
-      { "<auto>", mode = "nixsotc" },
-      { "t",      mode = { "n", "v" } },
-    }
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  }
+-- TODO: on clicking ', which-key is showing all bookmarks list, disable it
+--  "folke/which-key.nvim",
+--  event = "VeryLazy",
+--  opts = {
+--    -- https://github.com/folke/which-key.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
+--    triggers = {
+--      { "<leader>", mode = { "n", "v" } },
+--      { "t",        mode = { "n", "v" } },
+--    }
+--  },
+--  keys = {
+--    {
+--      "<leader>?",
+--      function()
+--        require("which-key").show({ global = false })
+--      end,
+--      desc = "Buffer Local Keymaps (which-key)",
+--    },
+--  }
 }

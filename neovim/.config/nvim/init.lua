0fbfc3ea9e38cf281c35ecb3f233def7d17c9b64
mahesh <maheshghamand@gmail.com>
2024-10-12 04:28:46 +0530

    neovim docs update

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -4,18 +4,22 @@
 --	 mv ~/.local/share/nvim{,.bak}
 --	 mv ~/.local/state/nvim{,.bak}
 --	 mv ~/.cache/nvim{,.bak}
--- 1. download Tarball: https://github.com/neovim/neovim/releases/tag/v0.10.0
+
+-- 1. download Tarball: https://github.com/neovim/neovim/releases/tag/v0.10.1
 -- 2. installation script: https://github.com/neovim/neovim/blob/master/INSTALL.md#linux
 -- nvim --version
--- :checkhealth
--- :help config, default path: ~/.config/nvim/init.lua
--- define config path: export $XDG_CONFIG_HOME=<path-name>, neovim will look for $XDG_CONFIG_HOME=/nvim/init.lua
+-- :checkhealth (will show if neovim detected config file)
+-- append `export XDG_CONFIG_HOME=/root/.config` to .bashrc file, if config is located at: /root/.config/nvim/init.lua
+--    verify path using `echo $XDG_CONFIG_HOME`
+--    neovim will look for $XDG_CONFIG_HOME=/nvim/init.lua
+--    :help config, default path: ~/.config/nvim/init.lua
 
 -- requirements:
 -- treesitter, harpoon, telescope, mason needs c compiler such as gcc,
 --    on ubuntu install compilers with: `sudo apt install build-essential`
 --    on nixos install `gcc` package
 -- tsserver needs npm to install packages
+--    on ubuntu install nodejs,npm through nvm: https://nodejs.org/en/download/package-manager
 -- https://github.com/BurntSushi/ripgrep for telescope
 -- eslint_d :Mason -> eslint_d -> i
 

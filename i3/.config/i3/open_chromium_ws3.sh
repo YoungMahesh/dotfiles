ddad3940cf6b92407a792714b9a6711fcade3783
mahesh <maheshghamand@gmail.com>
2025-06-29 15:26:10 +0530

    i3config update

--- i3/.config/i3/open_chromium_ws3.sh
+++ i3/.config/i3/open_chromium_ws3.sh
@@ -1,11 +1,11 @@
 # in first setup use - `chmod +x <file-name>`
-# switch/open workspace 9
-i3-msg "workspace number 9"
+# switch/open workspace 3
+i3-msg "workspace number 3"
 # `workspace number 9` and `chromium` run asynchronously, so your chromium script may execute before the workspace 
 # switch finishes which will lead to no opening of chromium in workspace 9, hence use sleep to add tiny delay
 #sleep 0.1
 
 # Try to focus existing Chromium window in workspace 9
-if ! i3-msg '[class="Chromium" workspace="9"] focus' 2>/dev/null; then
-    chromium --new-window --profile-directory="Default" "https://t3.chat"
+if ! i3-msg '[class="Chromium" workspace="3"] focus' 2>/dev/null; then
+    chromium --new-window --profile-directory="Default" 
 fi

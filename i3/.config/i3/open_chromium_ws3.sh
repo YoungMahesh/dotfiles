ab03bc9448c2e3aa011cd79809da673042587558
mahesh <maheshghamand@gmail.com>
2025-07-02 14:23:49 +0530

    update i3wm config

--- i3/.config/i3/open_chromium_ws3.sh
+++ i3/.config/i3/open_chromium_ws3.sh
@@ -1,11 +1,12 @@
 # in first setup use - `chmod +x <file-name>`
-# switch/open workspace 3
+# Switch to workspace 3  
 i3-msg "workspace number 3"
-# `workspace number 9` and `chromium` run asynchronously, so your chromium script may execute before the workspace 
-# switch finishes which will lead to no opening of chromium in workspace 9, hence use sleep to add tiny delay
-#sleep 0.1
 
-# Try to focus existing Chromium window in workspace 9
-if ! i3-msg '[class="Chromium" workspace="3"] focus' 2>/dev/null; then
-    chromium --new-window --profile-directory="Default" 
+# Count Chromium windows in workspace 3
+CHROMIUM_COUNT=$(i3-msg -t get_tree | jq '.nodes[] | .. | select(.type? == "workspace" and .name? == "3") | .. | select(.window_properties?.class? == "Chromium") | .window_properties.class' | wc -l)
+
+# Only launch Chromium if no Chromium windows exist in workspace 3
+if [ "$CHROMIUM_COUNT" -eq 0 ]; then
+    chromium --new-window --profile-directory="Default"
 fi
+

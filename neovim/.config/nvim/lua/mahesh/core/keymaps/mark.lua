909249d3a1fa075239fa7bf62ad6bb06e9cc37b7
Mahesh <maheshghamand@gmail.com>
2025-08-22 12:52:22 +0530

    store all nvim keymaps-info in one file

--- neovim/.config/nvim/lua/mahesh/core/keymaps/mark.lua
+++ neovim/.config/nvim/lua/mahesh/core/keymaps/mark.lua
@@ -1,15 +1,16 @@
 local keymap = vim.keymap
 --------------------- `:help m` `help: mark`--------------------------------
 -->m=markS
--- Map 'a-d to jump to mark position and center screen
+-- Map "a-d to jump to mark position and center screen
 -- string.byte() -> converts characters to their numeric ASCII/byte values, string.byte('a') == 97
 -- string.char() -> converts numeric ASCII/byte values to characters, string.char(97) == 'a'
 for mark = string.byte('a'), string.byte('d') do
-  keymap.set('n', "'" .. string.char(mark), '`' .. string.char(mark) .. 'zz', {noremap = true, silent = true})
+  keymap.set('n', '"' .. string.char(mark), '`' .. string.char(mark) .. 'zz', {noremap = true, silent = true})
 end
 
 keymap.set("n", "ml", "<cmd>marks abcdefABCDEF<cr>", { desc = "show marks list from a to f" })
 --keymap.set("n", "mm", "<cmd>marks ABCDE<cr>", { desc = "show marks list from a to f" })
+-- ------------- Default settings -------------------------
 -- `ma`: set mark a, `'a`: go to line of mark a, ``m`: go to position of mark, `['`: go to previous mark, `]'`: go to next mark
 -- `:delm A` to delete mark A, `:delm a` to delete mark a
 -- :marks: show all marks

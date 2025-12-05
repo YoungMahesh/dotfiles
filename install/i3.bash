6bbae1d06ff439780cea67adf381525a476448cf
mahesh <maheshghamand@gmail.com>
2025-12-05 09:43:35 +0530

    refactor i3 configuration

---
+++ install/i3.bash
@@ -0,0 +1,10 @@
+cd ~/dotfiles && stow i3
+
+sudo apt install i3
+i3 --version
+# i3 version 4.23 (2023-10-29) © 2009 Michael Stapelberg and contributors
+
+# Log out of your current session.
+# On the login screen, click the gear icon or session menu.
+# Select i3. -> Log in. 
+

b58c3bfcf751f6981ba949d92af19a554f5e29c9
mahesh <maheshghamand@gmail.com>
2026-02-22 15:58:07 +0530

    update antigravity keymaps

--- bash/.scripts/main.bash
+++ bash/.scripts/main.bash
@@ -2,7 +2,6 @@ source ~/.scripts/aliases.bash
 source ~/.scripts/bluetooth.bash
 source ~/.scripts/brightness.bash
 source ~/.scripts/fzf.bash
-source ~/.scripts/git.bash
 source ~/.scripts/history0.bash
 source ~/.scripts/path.bash
 source ~/.scripts/remaining.bash

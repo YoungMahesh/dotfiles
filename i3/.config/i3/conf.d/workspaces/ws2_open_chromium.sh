50108360599d93ad66ccf7d9b3fa2b0aeb0f06a6
mahesh <maheshghamand@gmail.com>
2026-03-06 12:22:26 +0530

    use google-chrome instead of chromium in workspace 2

--- i3/.config/i3/conf.d/workspaces/ws2_open_chromium.sh
+++ i3/.config/i3/conf.d/workspaces/ws2_open_chromium.sh
@@ -3,12 +3,12 @@
 i3-msg "workspace number 2"
 
 # Count Chromium windows in workspace 2
-CHROMIUM_COUNT=$(i3-msg -t get_tree | jq '.nodes[] | .. | select(.type? == "workspace" and .name? == "2") | .. | select(.window_properties?.class? == "Chromium") | .window_properties.class' | wc -l)
+CHROMIUM_COUNT=$(i3-msg -t get_tree | jq '.nodes[] | .. | select(.type? == "workspace" and .name? == "2") | .. | select(.window_properties?.class? == "Google-chrome") | .window_properties.class' | wc -l)
 
 # Only launch Chromium if no Chromium windows exist in workspace 2
 if [ "$CHROMIUM_COUNT" -eq 0 ]; then
     # workspace directory-names: 1st created - 'Default'; 2nd created: 'Profile 1'; 3rd created - 'Profile 2'
     # get profile-path by visiting "chrome://version/" inside chromium
-    chromium --new-window --profile-directory="Profile 2"
+    google-chrome --new-window --profile-directory="Profile 1"
 fi
 

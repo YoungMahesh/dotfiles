86c9ec823d4e0cfe9fba8337a692f35c38408652
mahesh <maheshghamand@gmail.com>
2026-07-04 16:07:27 +0530

    kitty: update config

--- bash/.scripts/fzf.bash
+++ bash/.scripts/fzf.bash
@@ -1,14 +1,12 @@
 ff() {
-    local custom_dirs=(~/dotfiles ~/Documents) 
+    local custom_dirs=(~/dotfiles ~/Documents)
     local dirs=$(
-        (find ~/company ~/personal ~/sync -maxdepth 1 -type d; 
-         printf '%s\n' "${custom_dirs[@]}") | 
+        (find ~/company ~/personal ~/sync -maxdepth 2 -type d;
+         printf '%s\n' "${custom_dirs[@]}") |
         fzf
     )
-    
+
     if [ -n "$dirs" ]; then
         cd "$dirs" && clear
     fi
 }
-
-

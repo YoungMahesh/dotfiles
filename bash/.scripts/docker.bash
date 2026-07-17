45d792a3801853dcd554b5638543d3558a41d085
mahesh <maheshghamand@gmail.com>
2026-07-17 19:19:09 +0530

    bash aliases for server

--- bash/.scripts/docker.bash
+++ bash/.scripts/docker.bash
@@ -1,4 +1,5 @@
-alias dc='docker container' 
+alias dc='docker container'
 alias dco='docker compose'
 alias di='docker image'
 alias dl='docker logs'
+alias dn='docker network'
---
+++ bash/.scripts/server.bash
@@ -0,0 +1,20 @@
+# --------------------------------------------------------------------------------
+# cat server.bash >> ~/.profile
+# docker
+alias dc='docker container'
+alias dco='docker compose'
+alias di='docker image'
+alias dl='docker logs'
+alias dn='docker network'
+
+# git
+alias gs='git status'
+alias gd='git diff'
+alias gds='git diff --cached'
+alias ga='git add .'
+alias gl='git log'
+alias gc='git commit -m'
+alias gp='git push'
+alias gP='git pull'
+alias gb='git branch'
+# --------------------------------------------------------------------------------

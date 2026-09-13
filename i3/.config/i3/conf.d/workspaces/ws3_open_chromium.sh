270b0fe5f0591e30daf53b7e812d48c370a43a09
mahesh <maheshghamand@gmail.com>
2026-09-13 20:22:09 +0530

    i3wm: move telegram to alt-5

--- i3/.config/i3/conf.d/workspaces/ws3_open_chromium.sh
+++ i3/.config/i3/conf.d/workspaces/ws3_open_chromium.sh
@@ -1,5 +1,5 @@
 # in first setup use - `chmod +x <file-name>`
-# Switch to workspace 3  
+# Switch to workspace 3
 # why 'google-chrome' instead of 'chromimum'?
 #   - can auto-sync browser-bookmarks with google-account with google-chrome but not with chromium
 i3-msg "workspace number 3"
@@ -12,10 +12,15 @@ CHROMIUM_COUNT=$(i3-msg -t get_tree | jq '.nodes[] | .. | select(.type? == "work
 
 # Only launch Chromium if no Chromium windows exist in workspace 3
 if [ "$CHROMIUM_COUNT" -eq 0 ]; then
-    # workspace directory-names: 1st created - 'Default' 
+    # workspace directory-names: 1st created - 'Default'
     # 2nd created: 'Profile 1'; 3rd created - 'Profile 2'
     # for chromium - chromium --new-window
     # for chrome - google-chrome --new-window
-    google-chrome --new-window --profile-directory="Default"
-fi
 
+    # before testing --force-device-scale-factor=1.2` argument, you need to all chrome instances using:
+    #   killall -9 chrome google-chrome google-chrome-stable 2>/dev/null
+    # due to chrome UI increase, webpages size also increases, to reduce that update settings in each user profile:
+    #   1. visit: chrome://settings/appearance
+    #   2. Page zoom: 90%
+    google-chrome --new-window --profile-directory="Default" --force-device-scale-factor=1.2
+fi
